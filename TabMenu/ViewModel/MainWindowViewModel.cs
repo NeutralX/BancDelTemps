0b6c8e6fb06e1cc4e485144a28b03c96c4850aad
Alex <Alex@DESKTOP-KH2V179>
2019-01-23 19:57:03 +0100

    INotifyPropertyChanged

---
+++ TabMenu/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BancDelTemps.Annotations;
+
+namespace BancDelTemps.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+
+    }
+}
