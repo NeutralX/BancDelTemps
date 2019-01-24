4609764826ba26d021c1b343cd687b3e243a3628
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2019-01-24 19:33:13 +0100

    Classes creades

---
+++ TabMenu/Model/Class/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace BancDelTemps.Model.Class
+{
+    public class Admin
+    {
+
+        public int Id_Admin { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ TabMenu/Model/Class/Pact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BancDelTemps.Model.Class
+{
+    public class Pact
+    {
+        public int Id_Pact { get; set; }
+        public DateTime? date_created { get; set; }
+        public DateTime? date_finished { get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+        public int Posts_Id_Post { get; set; }
+        public int Reports_Id_Report { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual Report Report { get; set; }
+    }
+}
