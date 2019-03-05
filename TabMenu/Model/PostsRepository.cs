290adc5746f0e46a41d748d85265ecd0e38b7fa2
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2019-03-05 20:14:24 +0100

    Posts no funcione en azure

--- TabMenu/Model/PostsRepository.cs
+++ TabMenu/Model/PostsRepository.cs
@@ -14,8 +14,8 @@ namespace BancDelTemps.Model
 {
     class PostsRepository
     {
-        //private static string ws1 = "https://wsbancdeltemps.azurewebsites.net/api/";
-        private static string ws1 = "http://localhost:60608/api/";
+        private static string ws1 = "https://wsbancdeltemps.azurewebsites.net/api/";
+        //private static string ws1 = "http://localhost:60608/api/";
 
         public static List<Post> GetAllPosts()
         {
@@ -39,7 +39,7 @@ namespace BancDelTemps.Model
 
         public static List<Post> GetPostsByDateFinished(DateTime dateFinished)
         {
-            List<Post> lp = (List<Post>)MakeRequest(string.Concat(ws1, "postsDateFinished/",dateFinished), null, "GET", "application/json", typeof(List<Post>));
+            List<Post> lp = (List<Post>)MakeRequest(string.Concat(ws1, "postsDateFinished/", dateFinished), null, "GET", "application/json", typeof(List<Post>));
             return lp;
         }
 
