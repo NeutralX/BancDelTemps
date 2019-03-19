c320f4400b2ce771c74e47df4b182b531f5b66f1
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2019-03-19 21:07:29 +0100

    Inici detall pacts

---
+++ TabMenu/ViewModel/ReportDetallViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using BancDelTemps.Model.Class;
+
+namespace BancDelTemps.ViewModel
+{
+    public class ReportDetallViewModel : INotifyPropertyChanged
+    {
+        #region NotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+        #region RelayCommand
+        class RelayCommand : ICommand
+        {
+            private Action<object> _action;
+
+            public RelayCommand(Action<object> action)
+            {
+                _action = action;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                if (parameter != null)
+                {
+                    _action(parameter);
+                }
+                else
+                {
+                    _action("Hello world");
+                }
+            }
+
+            public event EventHandler CanExecuteChanged;
+        }
+        #endregion
+
+        public Report Report { get; set; }
+
+        public ReportDetallViewModel()
+        {
+        }
+
+        public ReportDetallViewModel(Report report)
+        {
+            Report = report;
+        }
+    }
+}
